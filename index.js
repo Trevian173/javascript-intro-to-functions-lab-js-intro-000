1a8d5afb053304389edd65691b6292274c449cdc
Trevian Brown <trevian1966@gmail.com>
2021-02-05 09:10:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ function logWhisper(string){
 function sayHiToGrandma(string){
 if (string.toLowerCase()){
   return "I can't hear you"
-} else if (string.toUpperCase()) {
-  return "YES INDEED!"
-}else if (string.toEqual("I love you, Grandma")) {
-  return "I love you, too."
-}
+} 
 
 }
